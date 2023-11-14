eafaf77b16120a121fdbb51896c5a3302b0c986d
christopherkinyua <christopher.kinyua@ucalgary.ca>
2023-11-14 01:32:39 -0700

    Filled in the idle page

--- frontend/src/pages/Idle.tsx
+++ frontend/src/pages/Idle.tsx
@@ -1,9 +1,24 @@
-import { Box, Button } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
+import logo from '../assets/logo.png';
+
+const styles = {
+header:{
+  alignItems: "flex-start",
+}
+}
 
 const Idle = () => {
   return (
-    <Box>Idle
-      <Button>Hello</Button>
+    <Box>
+      <Box sx = {styles.header}>
+      <img 
+      src={logo} 
+      alt="BabelBot logo" 
+      style={{width: 100, height: 100, alignSelf: 'flex-start'}}/>
+      </Box>
+
+      <Box><Typography>Insert Avatar</Typography></Box>
+      <Button>Say "Hey BabelBot"</Button>
     </Box>
     
   )
