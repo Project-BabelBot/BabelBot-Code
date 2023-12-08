2e847a43806b98796bc1accf3c408a5ad197eb3b
Dennull <lieudennis7@gmail.com>
2023-12-07 21:39:12 -0700

    Add MUI theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import theme from "./theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
