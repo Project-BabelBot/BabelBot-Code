cfd9f8f08ab599d546f3da49bc16b527b59f252f
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-01-31 22:39:00 -0700

    Update VirtualKeyboard.tsx

--- frontend/src/components/VirtualKeyboard.tsx
+++ frontend/src/components/VirtualKeyboard.tsx
@@ -1,6 +1,22 @@
 import { Box, Typography } from "@mui/material";
 import Keyboard from "react-simple-keyboard";
 
+//For styling the keyboard
+const styles = {};
+
+//Function for when the key is pressed
+const onKeyPress = () => {};
+
+//Function for changing the layout based on the language detected
+const handleLanguage = () => {};
+
+//Function when the shift button is pressed
+const handleShift = () => {};
+
+/*Function when the Caps button is pressed.
+  Will have to change layout to upper case*/
+const handleCaps = () => {};
+
 const VirtualKeyboard = () => {
   return (
     <Box>
